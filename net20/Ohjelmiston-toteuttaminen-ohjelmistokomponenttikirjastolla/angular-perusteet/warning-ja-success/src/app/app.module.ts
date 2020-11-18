3f96939ef41fd265775e8c68f2e8b4dd8bdd6267
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2020-11-18 13:04:01 +0200

    Created new project and two new components

---
+++ net20/Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/warning-ja-success/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
