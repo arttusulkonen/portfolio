17a283ff9da1e91e5086f6a1570ea43c9e3d8d1b
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2020-11-25 12:16:27 +0200

    serverName Button alle

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-dream-app/src/app/app.module.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-dream-app/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { ServersComponent } from './servers/servers.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
