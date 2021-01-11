ef82de5a42d9ea47a313ed292afeccd7a946e488
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-11 15:31:29 +0200

    added sorting by name

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/app.component.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tuoteluettelo';
+  name: any;
+
+  constructor(private http: HttpClient){}
+  arrPhones: string[];
+
+  ngOnInit(): void {
+    this.http.get('../assets/phones.json').subscribe(data => {
+      this.arrPhones = data as string[];
+    },(err: HttpErrorResponse) => {
+      console.log(err.message);
+      }
+    );
+  }
+
+  Search(){
+    if(this.name === ''){
+      this.ngOnInit();
+    } else {
+      this.arrPhones = this.arrPhones.filter(res => {
+        // @ts-ignore
+        return res.name.toLocaleLowerCase().match(this.name.toLocaleLowerCase());
+      });
+    }
+  }
 }
+
--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/app.module.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/app.module.ts
@@ -2,30 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MotorolaComponent } from './motorola/motorola.component';
-import { DellComponent } from './dell/dell.component';
-import { LgComponent } from './lg/lg.component';
-import { DroidComponent } from './droid/droid.component';
-import { SamsungComponent } from './samsung/samsung.component';
-import { NexusComponent } from './nexus/nexus.component';
-import { TMobileComponent } from './t-mobile/t-mobile.component';
-import { SanyoComponent } from './sanyo/sanyo.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MotorolaComponent,
-    DellComponent,
-    LgComponent,
-    DroidComponent,
-    SamsungComponent,
-    NexusComponent,
-    TMobileComponent,
-    SanyoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
