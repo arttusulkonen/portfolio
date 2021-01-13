5aef9afcbeb9711699021693338c7c8987c9cbe1
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-13 11:10:50 +0200

    added new heroes

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
