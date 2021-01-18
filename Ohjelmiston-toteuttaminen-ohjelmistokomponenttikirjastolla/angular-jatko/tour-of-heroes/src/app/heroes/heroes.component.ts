2f713b8d788d841e293a6e7a67578a8c541e577d
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-18 11:09:44 +0200

    refactored data access to the HeroService class. registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app. created a MessageService for loosely-coupled communication between classes.

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +14,22 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
