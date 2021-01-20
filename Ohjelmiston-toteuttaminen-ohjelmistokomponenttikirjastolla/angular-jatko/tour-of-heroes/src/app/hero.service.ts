4984b63a056bac6ccb965beedc3325ecb49add78
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-01-20 10:27:17 +0200

    added navigation

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/hero.service.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tour-of-heroes/src/app/hero.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
+
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
