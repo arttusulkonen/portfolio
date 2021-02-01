328fc36c50cf2a50ce3fe07276cb8e61f85c35b3
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-02-01 09:39:23 +0200

    New project 'tuoteluettelo'

---
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
