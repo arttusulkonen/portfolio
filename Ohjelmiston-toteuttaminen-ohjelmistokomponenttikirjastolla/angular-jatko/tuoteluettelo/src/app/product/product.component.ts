e211c2f4f65ba09db507303ffc3666ae844df85e
Arttu Sulkonen <arttu.sulkonen@gmail.com>
2021-02-03 09:39:06 +0200

    Create new project

--- Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/product/product.component.ts
+++ Ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-jatko/tuoteluettelo/src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import phones from '../../assets/phones/phones.json';
+
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  phones = phones;
+  phone;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.phone = this.phones[+params.get('productId')];
+    });
   }
 
 }
